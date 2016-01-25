886caec9b8a65b04dab4736c80b37a897ee0b7f7
Matthew Phelps <mdp553@hotmail.com>
2016-01-25 15:01:10 +0100

    Data transfer from CPH project
    Set up script to transfer most recent data from CPH project

---
+++ Data-1_tansfer-from-CPH-project.R
@@ -0,0 +1,23 @@
+# Author: Matthew Phelps
+# Desc: Pull data from CPH project and store in local directory
+# Output datasets: Rdata in local directory
+
+## intro
+rm(list = ls())
+ifelse(grepl("wrz741", getwd()),
+       data.path <- "C:\\Users\\wrz741\\Google Drive\\Copenhagen\\DK Cholera\\CPH",
+       data.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH")
+
+ifelse(grepl("wrz741", getwd()),
+       save.path <- "C:\\Users\\wrz741\\Google Drive\\Copenhagen\\DK Cholera\\Cholera-DK-paper1\\Rdata\\Data-1_transfer-from-CPH-project",
+       save.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/Cholera-DK-paper1/Rdata/Data-1_transfer-from-CPH-project")
+
+setwd(data.path)
+
+load("Data\\Rdata\\all_age.Rdata")
+load('Data\\Rdata\\age1855.Rdata')
+load('Data\\Rdata\\age1850.Rdata')
+load("Data\\Rdata\\quarter_combined.Rdata")
+
+# Save in local directory
+save(age1850, age1855, all_age, combined, file = save.path)
