2abe1e0b1ce13f21eb756fae6b14adf451e41e6f
matthew-phelps <ma.phelps@gmail.com>
2016-05-18 09:44:17 -0400

    minor

--- coastal-town-risk.R
+++ coastal-town-risk.R
@@ -15,7 +15,7 @@ setwd(data.path)
 
 graphics.off()
 
-devtools::install_github('matthew-phelps/CholeraDataDK')
+# devtools::install_github('matthew-phelps/CholeraDataDK')
 library(ggplot2)
 library(rgdal)
 library(tidyr)
@@ -30,6 +30,10 @@ towns <- dk@data
 # Set towns with 1 cholera case (coded as: cholera == 3) to "no cholera"
 towns$cholera <- ifelse(towns$cholera == 3 | towns$cholera == 0, 0, 1)
 
+# # Set towns with "unkown" port status to have a port
+# towns$Port <- ifelse(towns$Port == 3, 1, 0)
+
+
 # Remove towns with unknown "Port" status
 towns <- towns[which(towns$Port != 3), 2:5]
 
@@ -39,7 +43,7 @@ table(large_towns[2:3])
 
 
 # CREATE VARIABLE ---------------------------------------------------------
-objt <- towns
+objt <- large_towns
 
 sick_exp <- nrow(objt[which(objt$Port == 1 & objt$cholera == 1), ])
 sick_not_exp <- nrow(objt[which(objt$Port == 0 & objt$cholera == 1), ])
@@ -60,3 +64,6 @@ fmsb::riskratio(sick_exp, sick_not_exp, pop_exp, pop_not_exp)
 
 (sick_exp / pop_exp) / (sick_not_exp / pop_not_exp)
 
+#####
+## NO ASSOCIATION BETWEEN PORT STATUS AND RISK OF CHOLERA ##
+#####
